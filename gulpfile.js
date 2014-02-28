1a4613c1a59975ef58e42711619519138084c5a0
Peter Andersson <sehnpaa@gmail.com>
2014-02-28 19:01:28 +0100

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var gulputil = require('gulp-util');
+var qunit = require('gulp-qunit');
+var jshint = require('gulp-jshint');
+
+var paths = {
+  scripts: ['app.js', 'test/test.js'],
+  testRunner: './test/index.html'
+};
+
+
+gulp.task('default', ['lint', 'test', 'watch']);
+	
+gulp.task('lint', function () {
+	gulp.src(paths.scripts)
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function () {
+	return gulp.src(paths.testRunner)
+		.pipe(qunit());
+});
+
+gulp.task('watch', function () {
+	gulp.watch(paths.scripts, ['test']);
+});
+
