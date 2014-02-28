1b3d7522012355949c9849f334c2ceea907fe7fb
Peter Andersson <sehnpaa@gmail.com>
2014-02-28 20:17:28 +0100

    comment out initBoard test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ test("sanity check", function () {
 	equal(4, 4);
 });
 
-test("initBoard", function () {
+/*test("initBoard", function () {
 	module.initBoard();
 	equal(module.cells[0][0].state, "white", "first och first state is white");
-});
+});*/
